1aa0395bb108ddc4b26fe929346c4693d9253d71
Mlodkoo <63650085+Mlodko@users.noreply.github.com>
2024-01-09 14:29:45 +0100

    Did some trolling
    Localized the database, it no longer relies on setting up a server, it uses sqlite. Added Maven package manager to the project and also some dependencies

--- WalletHelper/src/main/java/databaseAccess/DBConnection.java
+++ WalletHelper/src/main/java/databaseAccess/DBConnection.java
@@ -3,30 +3,26 @@ package databaseAccess;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 // This class is made for database connection management: it opens and closes the connection
 // with database and holds its current status (whether it is set or not).
 public abstract class DBConnection {
 
-    protected static void setConnection(/*String user, String pass*/) throws SQLException {
+    protected static void setConnection(/*String user, String pass*/){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establish a connection to the sqlite db
+            Connection connection = DriverManager.getConnection(databaseUrl);
+            // Create a statement object
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, phoneNumber TEXT, password TEXT)");
+            statement.close();
+            isSet = true;
+            mainConnector = connection;
         }
-        catch (ClassNotFoundException e) {
+        catch(SQLException e) {
             e.printStackTrace();
-            System.out.println("setConnection - no Driver: " + e.getMessage());
-            // log4j instructions
-            return;
         }
-
-        /*
-        username = user;
-        password = pass;
-         */
-
-        mainConnector = DriverManager.getConnection(databaseUrl, username, password);
-
-        isSet = true;
     }
 
     protected static void closeConnection() throws SQLException {
@@ -44,8 +40,13 @@ public abstract class DBConnection {
     }
 
     private static Connection mainConnector;
-    private static final String databaseUrl = "jdbc:mysql://localhost:3306/wallethelper_example";
+    private static final String databaseUrl = "jdbc:sqlite:D:\\dev\\java\\wallet-helper\\database.db";
     private static String username = "app";
     private static String password = "1app2Password3";
     private static boolean isSet = false;
+
+    public static void main(String[] args) {
+        setConnection();
+    }
 }
+
